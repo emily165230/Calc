c1da7ae010e5b26e28d4b639a567891e37e2b0ec
emily165230 <emilyr28110@gmail.com>
2024-12-26 10:27:38 +0200

    calc final

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.calc;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    public void onBtnClicked(View view) {
+
+        EditText et1 = findViewById(R.id.Num1);
+        String et1Text = et1.getText().toString();
+        Integer num1 = Integer.valueOf(et1Text);
+
+        EditText et2 = findViewById(R.id.Num2);
+        String et2Text = et2.getText().toString();
+        Integer num2 = Integer.valueOf(et2Text);
+
+        Integer result = null;
+        if (view.getId() == R.id.btnPlus)
+            result = num1 + num2;
+        if (view.getId() == R.id.btnMinus)
+            result = num1 - num2;
+        if (view.getId() == R.id.btnMult)
+            result = num1 * num2;
+        if (view.getId() == R.id.btnDiv)
+            result = num1 / num2;
+        if (result != null) {
+            TextView tvRes = findViewById(R.id.tvResult);
+            tvRes.setText(result.toString());
+        }
+
+
+
+    }
+}
